7c0c6e15f9fcae4944d864b49f5e911bede87c94
Tegan Jain <teganjain527@gmail.com>
2024-10-02 15:50:43 +0530

    fix card styling, init call to action buttons, add more cards i know the cards look terrible rn, baad me sexy banayenge

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,14 +7,20 @@ export const HeroSection = () => {
       <div className="hero-container">
         {/*<div className="small-tagline">We build projects</div>*/}
         <div className="hero-header-container">
-          <div><h1 className="main-header">For The People</h1></div>
-          <div><h1 className="main-header">By The People</h1></div>
+          <h1 className="main-header">For The People</h1>
+          <h1 className="main-header">By The People</h1>
           <p className="hero-description">
             Solving KGP's problems, one tech project at a time.
           </p>
+          <div className="hero-button-container">
+            <button className="project-button">See our Projects!</button>
+            <button className="about-button">Learn more about what we do!</button>
+          </div>
         </div>
         <div className="hero-card-container">
           <HeroCard imageSource={metakgpLogo} title="IQPS" description="Hi!!!" />
+          <HeroCard imageSource={metakgpLogo} title="WIMP" description="Hi!!!" />
+          <HeroCard imageSource={metakgpLogo} title="Naarad" description="Hi!!!" />
         </div>
       </div>
     </section>
