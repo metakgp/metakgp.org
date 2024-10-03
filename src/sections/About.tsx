ca1fc9c87d6f1bfdca33e3c47d245f58a5b76fe3
Tegan Jain <teganjain527@gmail.com>
2024-10-03 20:57:57 +0530

    init a about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,7 +1,23 @@
+
+
 export const AboutSection = () => {
   return (
     <div>
-
+      <h1>What is <span className="bold">meta</span><span className="black">KGP</span>?</h1>
+      <div className="about-container">
+        <div className="about-left">
+          <p>
+            MetaKGP is a loose association of engineers, hackers, artists, and students from IIT Kharagpur...
+          </p>
+        </div>
+        <div className="about-right">
+          <h3 className="quote">
+            "The biggest resource we have is the quality of people who choose to participate. That is what must be maximised at all times. Smart people don’t like drudgery, and are generally driven enough to find another way to do something they really want to do, if we get in their way."
+            <br />
+            - Vikrant Verma, MetaKGP Co-Founder
+          </h3>
+        </div>
+      </div>
     </div>
   )
 }
