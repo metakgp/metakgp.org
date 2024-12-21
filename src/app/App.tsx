cd205aa02e0a337e5ec6944d29387e89ab2166d7
Md Waqib Sk <rentusk.1970@gmail.com>
2024-12-21 19:36:12 +0530

    Added a footer section in the website

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { HeaderSection } from '../sections/Header';
 import { HeroSection } from '../sections/Hero';
 import { StatsSection } from '../sections/Statistics';
+import { FooterSection } from '../sections/Footer';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <HeaderSection />
       </header>
       <HeroSection />
       <StatsSection />
+      <FooterSection />
     </div>
   );
 }
