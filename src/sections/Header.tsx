41dca9fde02ff33ce9295e39df40ddf90aa976fd
Tegan Jain <teganjain527@gmail.com>
2025-02-15 00:12:09 +0530

    fix(hero): change the link to /about to a link to /contribute in the hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,14 +1,25 @@
+import React from "react";
+import "../styles/header.css";
+import { Link, useLocation } from "react-router-dom";
+
+const NavElement = ({ route, isActive }: { route: string, isActive: boolean }) => {
+  return (
+    <Link to={route} className={isActive ? `active-nav-element` : `nav-element`}>{route != "dc" ? route.charAt(0).toUpperCase() + route.slice(1) : "DC++"}</Link>
+  )
+}
+
 export const HeaderSection = () => {
+  const location = useLocation();
+  const pages = ["about", "projects", "contribute", "dc"];
+
   return (
     <div className="header">
-      {/*<img className="metakgp-logo=metakgp-logo-one-line" src={MetaKGPLogoOneLine} width="150rem" />*/}
       <h1 className='metakgp-logo' data-set="metaKGP"
       ><span className='bold'>meta</span><span className='black'>KGP</span></h1>
       <nav className="nav-bar">
-        <a href="/about" className="nav-element">About</a>
-        <a href="/projects" className="nav-element">Projects</a>
-        <a href="/contribute" className="nav-element">Contribute</a>
-        <a href="/dc" className="nav-element">DC++</a>
+        {pages.map((page) => (
+          <NavElement route={page} isActive={location.pathname.slice(1) == page} />
+        ))}
       </nav>
     </div>
   )
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,12 +1,9 @@
+import React from "react";
 import devFocus from "../assets/undraw_pair_programming_re_or4x.svg";
+import "../styles/hero.css";
 import { Link } from "react-router-dom";
 
 export const HeroSection = () => {
-  // <div className="hero-card-container">
-  //   <HeroCard imageSource={metakgpLogo} title="IQPS" description="Hi!!!" />
-  //   <HeroCard imageSource={metakgpLogo} title="WIMP" description="Hi!!!" />
-  //   <HeroCard imageSource={metakgpLogo} title="Naarad" description="Hi!!!" />
-  // </div>
   return (
     <section>
       <div className="hero-container">
@@ -17,12 +14,12 @@ export const HeroSection = () => {
             metaKGP is IIT Kharagpur's (open) community for open-source enthuasists. We are dedicated to solving KGP's problems, one tech project at a time.
           </p>
           <div className="hero-button-container">
-          <Link to="/projects">
-    <button className="project-button">See our Projects!</button>
-  </Link>
-  <Link to="/about">
-    <button className="about-button">Learn more about what we do!</button>
-  </Link>
+            <Link to="/projects">
+              <button className="project-button">See our Projects!</button>
+            </Link>
+            <Link to="/contribute">
+              <button className="contribute-button">Learn more about how to contribute!</button>
+            </Link>
           </div>
         </div>
         <div className="hero-image-container">
