170594356b5f78c749a81266b76bd2a78f9cfa27
Tegan Jain <teganjain527@gmail.com>
2025-02-24 17:19:49 +0530

    migrate from CRA to vite

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import About from "./pages/About";
 import Contribute from "./pages/Contribute";
 import DCPage from "./pages/DCPage";
 import Projects from "./pages/Projects";
-import reportWebVitals from "./tests/reportWebVitals";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
@@ -26,8 +25,3 @@ root.render(
     </Routes>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
