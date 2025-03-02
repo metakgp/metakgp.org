a8c5daf65c6382e9793d01ea7d8f512c153258f9
Tegan Jain <teganjain527@gmail.com>
2025-03-02 22:56:36 +0530

    feat(projects): python script to fetch repo data

---
+++ get_repos.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+ORG_NAME = 'metakgp'
+
+def get_repositories(org_name):
+    url = f'https://api.github.com/orgs/{org_name}/repos'
+    
+    repos = []
+    
+    response = requests.get(url, params={'sort':'updated', 'per_page': 100}) # we only have like 70 repos so don't need to handle pagination yet
+    
+    if response.status_code != 200:
+        print(f"Error fetching repositories: {response.status_code} - {response.text}")
+        return repos
+    
+    data = response.json()
+    
+    for repo in data:
+        repos.append({
+            'name': repo['name'],
+            'description': repo['description'],
+            'stars': repo['stargazers_count'],
+            'forks': repo['forks_count'],
+            'language': repo['language']
+        })
+    
+    return repos
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+    print(f"Data saved to {filename}")
+
+if __name__ == '__main__':
+    repositories = get_repositories(ORG_NAME)
+    save_to_json(repositories, "src/data/repo_data.json")
+
