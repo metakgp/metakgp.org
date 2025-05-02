c176d4798a9b256a9dea4d55383e587dc0c5de85
Tegan Jain <teganjain527@gmail.com>
2025-05-02 13:55:10 +0530

    feat: add carousel

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import "../styles/components/Carousel.css"
+
+const Carousel = (props) => {
+  const { children } = props;
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [length, setLength] = useState(children.length)
+
+  // Set the length to match current children from props
+  useEffect(() => {
+    setLength(children.length)
+  }, [children])
+
+  const next = () => {
+    if (currentIndex < (length - 3)) {
+      setCurrentIndex(prevState => prevState + 1)
+    }
+  }
+
+  const prev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(prevState => prevState - 1)
+    }
+  }
+
+  return (
+    <div className="carousel-container">
+      <div className="carousel-wrapper">
+        <button className="left-arrow" onClick={prev}>
+          &lt;
+        </button>
+        <div className="carousel-content-wrapper">
+          <div className="carousel-content" style={{ transform: `translateX(-${currentIndex * 30}%)` }}>
+            {children}
+          </div>
+        </div>
+        <button className="right-arrow" onClick={next}>
+          &gt;
+        </button>
+      </div>
+    </div >
+  )
+}
+
+export default Carousel;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import { HeroSection } from "../sections/Hero";
 import { StatsSection } from "../sections/Statistics";
 // import CardGrid from '../sections/CardGrid';
+import Carousel from "../components/Carousel";
 import RepoData from "../data/repo_data.json";
 import { FEATURED_REPO, REPO_DATA_TYPE } from '../utils/types';
 import "../styles/components/FeaturedDisplay.css"
+import RepoCard from '../components/GithubCard';
 
 function About() {
   const Repos: REPO_DATA_TYPE[] = RepoData as REPO_DATA_TYPE[];
-  // const featuredReposList: string[] = ["iqps-go", "metakgp.org"];
-  // const featuredRepos: REPO_DATA_TYPE[] = Repos.filter(repo => featuredReposList.includes(repo.name)) ?? [{
-  //   name: "ERROR",
-  //   description: "You seem to have inputted a wrong repo name in the featured repos list, please check that your names are valid",
-  //   stars: 69,
-  //   forks: 420,
-  //   language: "ERROR"
-  // }];
+  const featuredReposList: string[] = ["iqps-go", "metakgp.org", "travel-buddy", "metakgp-wiki", "gyft", "naarad"];
+  const featuredRepos: REPO_DATA_TYPE[] = Repos.filter(repo => featuredReposList.includes(repo.name)) ?? [{
+    name: "ERROR",
+    description: "You seem to have inputted a wrong repo name in the featured repos list, please check that your names are valid",
+    stars: 69,
+    forks: 420,
+    language: "ERROR"
+  }];
 
   const featuredRepo: FEATURED_REPO = {
     screenshot_img: "http://0x0.st/84N_.png",
@@ -51,6 +53,11 @@ function About() {
             </div>
           </div>
         </div>
+        <Carousel>
+          {featuredRepos.map((repo, index) => (
+            <RepoCard repoData={repo} key={index} />
+          ))}
+        </Carousel>
       </section>
       <section className="topic-section">
         <h2 className="section-header-left">About MetaKGP</h2>
