f242e4650789a9d4c24413f992ed29e001c72eec
Tegan Jain <teganjain527@gmail.com>
2025-06-14 17:11:48 +0530

    feat: add about section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import GroupPicture from "../assets/metakgp_group_picture.jpeg";
+import MetaSpringImage from "../assets/metasprint.jpeg";
 import { HeroSection } from "../sections/Hero";
 import { StatsSection } from "../sections/Statistics";
 import FeaturedSection from '../sections/FeaturedDisplay';
+import "../styles/pages/About.css";
 
 function About() {
 
@@ -11,9 +14,32 @@ function About() {
       <StatsSection />
       <FeaturedSection />
       <section className="topic-section">
-        <h2 className="section-header-left">About MetaKGP</h2>
+        <h2 className="section-header-left">About Us</h2>
         <p className="description">
-          About metaKGP! Add a nice a writeup and images here!
+          Metakgp is an <strong>open, student-driven</strong> community at IIT Kharagpur dedicated to improving campus life by sharing knowledge and creating innovative projects. Students from all walks of life come together to <strong>document experiences, build tools, and create resources</strong> that benefit everyone at MetaKGP.
+        </p>
+
+        <div className='image-container'>
+          <img src={GroupPicture} className='metakgp-image' alt='metakgp group picture' />
+          <figcaption>
+            A group picture of some active metakgp maintainers <br />
+            From left to right: Rohan Barsagade, Arpit Bhardwaj, Harsh Khandeparkar, Chirag Ghosh, Shikhar Soni, and Rajiv Harlalka
+          </figcaption>
+        </div>
+
+        <p className='description'>
+          Metakgp hosts <strong>Metasprints</strong> — focused, collaborative sessions where members come together to brainstorm, develop, and enhance projects. These events are a great way for anyone, regardless of experience, to get involved and make a real impact.
+        </p>
+
+        <div className='image-container'>
+          <img src={MetaSpringImage} className='metakgp-image' alt='metasprint' />
+          <figcaption>
+            A metasprint in progress
+          </figcaption>
+        </div>
+
+        <p className='description'>
+          If you’re passionate about contributing, learning, or simply making campus life better, join Metakgp and help shape a more open and supportive IIT Kharagpur community!
         </p>
       </section>
     </div>
